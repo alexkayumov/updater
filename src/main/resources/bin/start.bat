b517997382a30c1dae4638b009d38debccecd1eb
aleksey <beckham475@mail.ru>
2019-04-14 17:19:31 +0300

    updater 14.04.2019

--- src/main/resources/bin/start.bat
+++ src/main/resources/bin/start.bat
@@ -1,2 +1,9 @@
-set workdir=%~dp0..\
-"%JAVA_HOME%\bin\java" -cp %workdir%\lib\* src.Main
+echo off
+chcp 1251
+set WORK_DIR=%~dp0..\
+
+@: -- раскоментировать и указать путь к JDK
+set JAVA_HOME=
+if "%JAVA_HOME%"=="" (echo Не задан путь к java) else (
+"%JAVA_HOME%\bin\java" -cp %WORK_DIR%lib\* src.Main
+)
